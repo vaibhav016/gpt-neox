da37ac0d7f73d27a853b4d8b0c7fab44f542b913
vaibhav016 <enriquecool8@gmail.com>
2024-03-26 17:02:24 -0400

    adding train, val data; integrating tensorboard train.py

--- megatron/neox_arguments/neox_args.py
+++ megatron/neox_arguments/neox_args.py
@@ -467,6 +467,16 @@ class NeoXArgsModel(NeoXArgsTemplate):
     Parameter controlling whether the output layer is parallelized over the hidden dim (row) or the vocab dim (column)
     """
 
+    identifier_string: str = ""
+    """
+    an identifier for the model, used for saving checkpoints,logging, etc.
+    """
+
+    warup_eval_interval: int = 50
+    """
+    the evaluation interval to use during warmup
+    """
+
 
 @dataclass
 class NeoXArgsOptimizer(NeoXArgsTemplate):
@@ -1213,7 +1223,16 @@ class NeoXArgsTraining(NeoXArgsTemplate):
     """
     What to scale width by when creating the delta model for mup
     """
+    
+    train_dataset_name: str = "no_train_dataset_name_given"
+    """
+    An identified for the training dataset used for logging
+    """
 
+    val_dataset_name: str = "no_val_dataset_name_given"
+    """
+    An identified for the training dataset used for logging
+    """
 
 @dataclass
 class NeoXArgsTextgen(NeoXArgsTemplate):
