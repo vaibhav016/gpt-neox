7fea82631c259e39845413be052c332969ac118c
pariyamd <pa.mehrbod@gmail.com>
2024-06-04 20:11:25 -0400

    setting up on beluga

--- sbatch_train_german_cosine_decay_replay.sh
+++ sbatch_train_german_cosine_decay_replay.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name="cosine_decay"
+#SBATCH --job-name="cosine_decay_lg_replay0p12"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=a100l:4
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=80G
 #SBATCH --time=06:00:00 
-#SBATCH --output=cosine_decay_lg_%j.out
-#SBATCH --error=cosine_decay_lg_%j.err
+#SBATCH --output=cosine_decay_lg_replay0p12_%j.out
+#SBATCH --error=cosine_decay_lg_replay0p12_%j.err
 
 
 # Some potentially useful distributed environment variables
@@ -20,11 +20,12 @@ export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 
 source write_hostfile.sh
 # Tell DeepSpeed where to find our generated hostfile via DLTS_HOSTFILE
-export DLTS_HOSTFILE=/home/mila/p/paria.mehrbod/scratch/resetbranch/gpt-neox/hostfiles/hosts_$SLURM_JOBID
+export DLTS_HOSTFILE=/home/pmehrbod/projects/def-eugenium/pmehrbod/gpt-neox/hostfiles/hosts_$SLURM_JOBID
 
-module load anaconda/3
-conda activate llm_project
-module load cuda/12.1.1 
+
+module load StdEnv/2020  gcc/9.3.0  cuda/11.7 python/3.9 arrow/13.0.0
+source ~/llm_proj_cuda117/bin/activate
+
 # export SLURM_TMPDIR=/Tmp/slurm.$SLURM_JOB_ID.0
 # export CUDA_VISIBLE_DEVICES=0
 echo $CUDA_VISIBLE_DEVICES
@@ -33,8 +34,8 @@ echo $CUDA_VISIBLE_DEVICES
 # echo "save_path: $SLURM_TMPDIR/output"
 # sd=$SLURM_TMPDIR/output
 
-# pile training
-python3 deepy.py train.py ./configs/49M_local_test.yml ./configs/pile_schedules/train/local_setup_pile_train.yml ./configs/schedules/cosine_decay_schedules/adam_cosine_lr3e-3_3e-6_wu-0.01.yml
+# german training
+python3 deepy.py train.py ./configs/49M_local_test_finetune.yml ./configs/german_schedules/pile_valid/local_setup_german_val_gr_and_pile_cosine_decay_replay.yml ./configs/schedules/cosine_decay_schedules/adam_cosine_lr3e-3_3e-6_wu-0.01.yml
 
 # cp -r $SLURM_TMPDIR/output $SCRATCH 
 
--- write_hostfile.sh
+++ write_hostfile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 GPUS_PER_NODE=4
-mkdir -p /home/mila/p/paria.mehrbod/scratch/resetbranch/gpt-neox/hostfiles
+mkdir -p /home/pmehrbod/projects/def-eugenium/pmehrbod/gpt-neox/hostfiles
 # need to add the current slurm jobid to hostfile name so that we don't add to previous hostfile
-hostfile=/home/mila/p/paria.mehrbod/scratch/resetbranch/gpt-neox/hostfiles/hosts_$SLURM_JOBID
+hostfile=/home/pmehrbod/projects/def-eugenium/pmehrbod/gpt-neox/hostfiles/hosts_$SLURM_JOBID
 # be extra sure we aren't appending to a previous hostfile
 rm $hostfile &> /dev/null
 # loop over the node names
